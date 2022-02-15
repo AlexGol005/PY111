0ad619601bc2bdc9c4fb33ca6aa281ed30ad9dc1
AlexGol005 <pesok1985>
2022-02-15 18:17:58 +0300

    init a0

--- Tasks/b0_linear_search.py
+++ Tasks/b0_linear_search.py
@@ -11,5 +11,12 @@ def min_search(arr: Sequence) -> int:
     :param arr: Array containing numbers
     :return: index of first occurrence of minimal element in array
     """
-    print(arr)
-    return -1
+
+    i = 0
+    min_ = arr[i]
+    while i < len(arr):
+        if arr[i] >= min_:
+            i += 1
+        elif arr[i] < min_:
+            min_ = arr[i]
+            return i
