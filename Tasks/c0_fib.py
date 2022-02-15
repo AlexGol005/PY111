2e1b8247db2e74cd52dc0a06e673ffa19b82b9e9
AlexGol005 <pesok1985>
2022-02-15 20:29:55 +0300

    c0  done

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -5,16 +5,49 @@ def fib_recursive(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    if n < 0:
+        raise  ValueError
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib_recursive(n - 1) + fib_recursive(n - 2)
 
 
-def fib_iterative(n: int) -> int:
+def fib_iterative(n: int) -> int: #O(n)
     """
     Calculate n-th number of Fibonacci sequence using iterative algorithm
 
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 1
+    if n < 0:
+        raise  ValueError
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    a = 0
+    b = 1
+    for _ in range(n - 1):
+        # sum_ = a + b
+        # a = b
+        # b = sum_
+        a, b = b, a + b
+    return b
+
+def generator_fib(n):
+    a = 0
+    yield a
+    b = 1
+    yield b
+    for _ in range(n - 2):
+        a, b = b, a + b
+        yield b
+N = 10
+list_ = [fib_iterative(i) for i in range(N)] #O(n**2)
+list1_ = [j for j in generator_fib(N)] #O(n)
+
+print(list_)
+print(list1_)
+
