aa01faf0289e931daa2739276b29663615ce00be
AlexGol005 <pesok1985>
2022-03-02 21:31:43 +0300

    b0 done

--- Tasks/b0_linear_search.py
+++ Tasks/b0_linear_search.py
@@ -1,5 +1,6 @@
 """
 This module implements some functions based on linear search algo
+Этот модуль реализует некоторые функции, основанные на алгоритме линейного поиска
 """
 from typing import Sequence
 
@@ -10,13 +11,21 @@ def min_search(arr: Sequence) -> int:
 
     :param arr: Array containing numbers
     :return: index of first occurrence of minimal element in array
+    Функция, которая находит минимальный элемент в массиве
+
+    :param arr: Массив, содержащий числа
+    :return: индекс первого появления минимального элемента в массиве
     """
 
     i = 0
     min_ = arr[i]
     while i < len(arr):
-        if arr[i] >= min_:
-            i += 1
-        elif arr[i] < min_:
+        if arr[i] < min_:
             min_ = arr[i]
-            return i
+            i += 1
+        elif arr[i] >= min_:
+            i += 1
+    return arr.index(min_)
+arr = [2, 1, 2, -1, -11, 2, 7,-10, 7, -12, 8]
+print(arr)
+print(min_search(arr))
